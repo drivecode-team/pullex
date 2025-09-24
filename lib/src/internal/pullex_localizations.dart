5addcc6ee7fb0d1886e2b74e4810e9ced84208a6
Erik Pettersson <eripe970@gmail.com>
2025-09-24 18:23:38 +0200

    Update pullex_localizations.dart
    Update with improved english translations

--- lib/src/internal/pullex_localizations.dart
+++ lib/src/internal/pullex_localizations.dart
@@ -215,10 +215,10 @@ class EnRefreshString implements RefreshString {
   String? canTwoLevelText = "Release to enter secondfloor";
 
   @override
-  String? idleLoadingText = "Pull up Load more";
+  String? idleLoadingText = "Pull up to load more";
 
   @override
-  String? idleRefreshText = "Pull down Refresh";
+  String? idleRefreshText = "Pull down to refresh";
 
   @override
   String? loadFailedText = "Load Failed";
